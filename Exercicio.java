0e76fb779c2f53c61b0d84d9598c386ec9c5dbc2
Tai <taiarachase@gmail.com>
2022-12-09 16:35:49 -0300

    Initial commit

---
+++ Exercicio.java
@@ -0,0 +1,34 @@
+package Array;
+
+import java.util.Arrays;
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+
+		double[] notasAlunoA = new double[4];
+		
+		notasAlunoA[0] = 7.9; 
+		notasAlunoA[1] = 8; 
+		notasAlunoA[2] = 6.7; 
+		notasAlunoA[3] = 9.7; 
+		
+		System.out.println(Arrays.toString(notasAlunoA));
+		
+		double totalAlunoA = 0;
+		for(int i = 0; i < notasAlunoA.length; i++) {
+			totalAlunoA += notasAlunoA[i];
+		}
+	
+		System.out.printf("A média do aluno é %.1f\n", totalAlunoA / 4);
+		
+		double[] notasAlunoB = { 7.9, 8.9, 6.7, 9.7 };
+		
+		double totalAlunoB = 0;
+		for (int i = 0; i < notasAlunoB.length; i++) {
+			totalAlunoB += notasAlunoB[i];
+		}
+		System.out.printf("A média do aluno é %.1f", totalAlunoB / 4);
+	}
+
+}
